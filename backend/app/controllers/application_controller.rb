a70d848f0b14b7717c7f5b37cfff50cefba091fb
Jesper Bränn <brann.jesper@gmail.com>
2016-10-08 17:01:42 +0200

    backend: make it possible to authenticate

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::API
   def home
     render json: {name: 'Studs 2017', api_version: '0.1'}.to_json
   end
+
+  protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |email, password|
+      user = User.find_by_email(email)
+      user && user.authenticate(password)
+    end
+  end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   # type_of_user: [:studs_member, :studs_substitute, :company]
+  has_secure_password
 end

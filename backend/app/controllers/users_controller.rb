7e64b72e656f076c9a179b79b1966d44612aff5e
Sara Feychting <sarafe@kth.se>
2016-11-01 14:26:44 +0100

    Added phone, position and master to users

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :enabled, :type_of_user)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :enabled, :type_of_user, :phone, :position, :master)
   end
 end

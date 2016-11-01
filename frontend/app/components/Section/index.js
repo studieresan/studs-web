14b65c92784c8e43f800a9412d2f0e2a31e79a08
jakobivarsson <jakob.ivarsson94@gmail.com>
2016-11-01 17:32:24 +0100

    home part 3

--- frontend/app/components/Section/index.js
+++ frontend/app/components/Section/index.js
@@ -1,10 +1,11 @@
 import { FormattedMessage } from 'react-intl';
 import React, { PropTypes } from 'react';
+import styles from './styles.css';
 
 function Section(props) {
 	return (
 	  <div>
-		<h2>
+		<h2 className={styles.header}>
 		  <FormattedMessage {...props.header} />
 		  <br/>
 		  {'-'}

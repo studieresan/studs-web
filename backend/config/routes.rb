903aa20d81e3eb95783408de0f3fc0b2c6cd3d30
Jesper Bränn <brann.jesper@gmail.com>
2016-11-17 13:43:57 +0100

    Add users show and own path

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :events
   root to: 'application#home'
 
+  get '/users/me', to: 'users#me', as: 'me'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

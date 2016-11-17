b5fd011c20d7b46f54d7c684c74544be0ebabca0
Jesper Bränn <brann.jesper@gmail.com>
2016-11-17 17:15:52 +0100

    Add user show and current user paths

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'application#home'
 
   resources :users
+  get '/users/me', to 'users#me', as: 'me'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

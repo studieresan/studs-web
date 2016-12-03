120838b4ef3a2cead878121b7cc8c82c4975ea16
Jesper Bränn <brann.jesper@gmail.com>
2016-12-03 17:58:32 +0100

    Add resume controller

---
+++ backend/app/controllers/resumes_controller.rb
@@ -0,0 +1,37 @@
+class ResumesController < ApplicationController
+  before_filter :authenticate
+
+  def show
+    if current_user.has_permission?('resume_get')
+      resume = Resume.find(params[:resume_id])
+      return render json: resume
+    end
+
+    render json: { error: 'Not allowed'}, status: 403
+  end
+
+  def create
+    resume = Resume.new(resume_params)
+    if resume.save
+      return render json: resume
+    end
+
+    render json: { error: '' }, status: 400
+  end
+
+  def update
+    resume = Resume.find(params[:resume_id])
+    if (current_user == resume.user || current_user.has_permission?('user_edit'))
+      if resume.update(resume_params)
+        return render json: resume
+      end
+    end
+
+    render json: { error: '' }, status: 403
+  end
+
+  private
+  def resume_params
+    params.require(:resume).permit()
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   get '/users/me', to: 'users#me', as: 'me'
   resources :users
-  get '/users/me', to 'users#me', as: 'me'
+  resources :resumes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

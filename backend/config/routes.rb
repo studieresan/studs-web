4ec8c2ffeb5a2542530f21a61da315af667a55b3
Jesper Bränn <brann.jesper@gmail.com>
2016-12-04 22:59:22 +0100

    Add simplifed patch route for resumes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/users/me', to: 'users#me', as: 'me'
   resources :users do
     get '/resume', to: 'resume#show'
+    patch '/resume', to: 'resume#update'
     resources :resume
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

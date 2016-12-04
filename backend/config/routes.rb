ce742abeb24dc25e1b4cd897ea4dc39ae626afca
Jesper Bränn <brann.jesper@gmail.com>
2016-12-04 16:12:46 +0100

    Nest the resume routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'application#home'
 
   get '/users/me', to: 'users#me', as: 'me'
-  resources :users
-  resources :resumes
+  resources :users do
+    resources :resume
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

fc793ce4196ae7caf8e72a47451554cf6cfa991f
jakobivarsson <jakob.ivarsson94@gmail.com>
2016-12-19 16:59:03 +0100

    make navbar responsive again

--- frontend/app/containers/Navbar/index.js
+++ frontend/app/containers/Navbar/index.js
@@ -12,9 +12,11 @@ import Isvg from 'react-inlinesvg';
 import styles from './styles.css';
 import LocaleToggle from '../LocaleToggle'
 import Logo from '../../static/img/studs-text.png';
+import MenuIcon from './icon_menu.svg';
+import CloseIcon from './icon_close.svg';
 
 import { loggedIn } from '../../auth';
-import { Link } from 'react-router';
+import { Link, withRouter } from 'react-router';
 import messages from './messages';
 import { connect } from 'react-redux';
 import User from '../User';
@@ -24,6 +26,20 @@ export class Navbar extends React.Component { // eslint-disable-line react/prefe
   constructor(props) {
     super(props);
     this.menu = this.menu.bind(this);
+    this.handleMenuClick = this.handleMenuClick.bind(this);
+    this.state = {
+      collapsed: true,
+    };
+  }
+
+  handleMenuClick() {
+    this.setState({collapsed: !this.state.collapsed});
+  }
+
+  componentWillMount() {
+    this.props.router.listenBefore(() => {
+      this.setState({collapsed: true});
+    });
   }
 
   menu() {
@@ -34,9 +50,12 @@ export class Navbar extends React.Component { // eslint-disable-line react/prefe
           <li>
             <Link to="/logout"><FormattedMessage {...messages.logout} /></Link>
           </li>
-          <li>
-            <Link to="/user">{ user.firstName }</Link>
-          </li>
+          { user.type === 'studs_member' ?
+            <li>
+              <Link to="/user">{ user.firstName }</Link>
+            </li>
+            : null
+          }
           <li>
             <Link to="/members"><FormattedMessage {...messages.studsmembers} /></Link>
           </li>
@@ -54,11 +73,21 @@ export class Navbar extends React.Component { // eslint-disable-line react/prefe
   }
 
   render() {
+    let collapsed = styles.collapse;
+    collapsed += this.state.collapsed ? ' ' + styles.collapsed : '';
     return (
 	  <div className={styles.navbar}>
-      <Link to="/"><img src={Logo} height={24} /></Link>
-      { this.menu() }
-      <LocaleToggle />
+      <div className={styles.control}>
+        <Link to="/"><img src={Logo} height={24} /></Link>
+        <img 
+          src={this.state.collapsed ? MenuIcon : CloseIcon }
+          height={26}
+          onClick={this.handleMenuClick} />
+      </div>
+      <div className={ collapsed }>
+        { this.menu() }
+        <LocaleToggle />
+      </div>
 	  </div>
     );
   }
@@ -74,6 +103,6 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(actions, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navbar));
 
 

d2460c489ffb3b7003f2f4c33e09c9b968e4699d
Jesper Bränn <brann.jesper@gmail.com>
2017-02-27 21:40:22 +0100

    Fix event company relation

--- backend/app/models/event.rb
+++ backend/app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_one :company
+  belongs_to :company
   has_many :users
   has_many :before_forms, -> { where type_of_form: 'before' }, class_name: 'UserEventForm'
   has_many :after_forms, -> { where type_of_form: 'after' }, class_name: 'UserEventForm'

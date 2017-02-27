2f24477ee9162501c2ef5449086181d5a076f52d
Jesper Bränn <brann.jesper@gmail.com>
2017-02-27 21:20:33 +0100

    Add permissions to user serialization

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :position, :master, :picture, :type_of_user
+  attributes :id, :first_name, :last_name, :email, :phone, :position, :master, :picture, :type_of_user, :permissions
 end

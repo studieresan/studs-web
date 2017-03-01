74630b5d832cbed70a031f532aba2cb95d35885e
Jesper Bränn <brann.jesper@gmail.com>
2017-03-01 18:45:59 +0100

    Route user event forms

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   resources :events
   resources :companies
+  resources :user_event_form
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

9fa38ec5f7f2094d5aaf7a318df52981df9fe654
Jesper Bränn <brann.jesper@gmail.com>
2017-03-01 18:32:10 +0100

    Set event properly

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
+require "google_drive"
 class EventsController < ApplicationController
   before_action :authenticate
-  require "google_drive"
+  before_action :set_event
+
 
   # GET /events
   # GET /events.json
@@ -34,7 +36,7 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
-    if event.update(event_params)
+    if @event.update(event_params)
       render json: event
     else
       render json: { error: '' }, status: 403
@@ -62,7 +64,7 @@ class EventsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
-      event = Event.find(params[:id])
+      @event = Event.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

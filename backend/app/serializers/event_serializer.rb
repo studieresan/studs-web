7c52da7bcaea5db4a71c8a40f196420e9e4efe55
Jesper Bränn <brann.jesper@gmail.com>
2017-03-01 17:32:27 +0100

    More event serialization

--- backend/app/serializers/event_serializer.rb
+++ backend/app/serializers/event_serializer.rb
@@ -1,3 +1,3 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :schedule, :information, :company
+  attributes :id, :schedule, :information, :company, :before_form_id, :after_form_id, :company_id, :before_form_url, :after_form_url, :responsible_user_id
 end

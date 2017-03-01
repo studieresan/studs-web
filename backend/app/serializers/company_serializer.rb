1c055f98063cd0c9b68e90afc3ccee18b51b3160
Jesper Bränn <brann.jesper@gmail.com>
2017-03-01 18:11:29 +0100

    Also serve company id

--- backend/app/serializers/company_serializer.rb
+++ backend/app/serializers/company_serializer.rb
@@ -1,3 +1,3 @@
 class CompanySerializer < ActiveModel::Serializer
-  attributes :name, :email, :phone, :address, :postal, :city, :country, :language, :information, :created_at, :updated_at
+  attributes :id, :name, :email, :phone, :address, :postal, :city, :country, :language, :information, :created_at, :updated_at
 end

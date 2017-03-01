ea55a2acc102a199387fe3a7c50bb446ab9cf899
Jesper Bränn <brann.jesper@gmail.com>
2017-03-01 19:07:12 +0100

    Route missing before/after event forms

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   get '/users/me', to: 'users#me', as: 'me'
   get '/users/password-reset', to: 'users#reset_password'
   patch '/users/password-reset', to: 'users#update_by_token'
+  get '/users/missing_before_forms', to: 'users#missing_before_forms'
+  get '/users/missing_after_forms', to: 'users#missing_after_forms'
   resources :users do
     get '/resume', to: 'resumes#show'
     patch '/resume', to: 'resumes#update'
   end
+
   resources :users
 
   resources :events

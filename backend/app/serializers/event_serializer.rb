e7c3602f15f88a424a95a00e64aea213de5bd3ec
Jesper Bränn <brann.jesper@gmail.com>
2017-04-27 19:55:08 +0200

    Serialize pictures

--- backend/app/serializers/event_serializer.rb
+++ backend/app/serializers/event_serializer.rb
@@ -1,3 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :schedule, :information, :company, :before_form_id, :after_form_id, :company_id, :before_form_url, :after_form_url, :before_form_replied, :after_form_replied, :responsible_user_id, :date, :public_text, :feedback_text, :formdata
+  attributes :id, :schedule, :information, :company, :before_form_id, :after_form_id,
+    :company_id, :before_form_url, :after_form_url, :before_form_replied, :after_form_replied,
+    :responsible_user_id, :date, :public_text, :feedback_text, :formdata, :picture_1, :picture_2, :picture_3
 end

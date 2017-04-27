7dfa3d8dd58779745d69adbc44441f1b17c0953c
Jesper Bränn <brann.jesper@gmail.com>
2017-04-27 20:35:08 +0200

    Default url paperclip

--- backend/app/models/event.rb
+++ backend/app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ApplicationRecord
   attr_accessor :before_form_replied, :after_form_replied
 
-  has_attached_file :picture_1
-  has_attached_file :picture_2
-  has_attached_file :picture_3
+  has_attached_file :picture_1, default_url: ''
+  has_attached_file :picture_2, default_url: ''
+  has_attached_file :picture_3, default_url: ''
   validates_attachment_content_type :picture_1, content_type: /\Aimage\/.*\z/
   validates_attachment_content_type :picture_2, content_type: /\Aimage\/.*\z/
   validates_attachment_content_type :picture_3, content_type: /\Aimage\/.*\z/

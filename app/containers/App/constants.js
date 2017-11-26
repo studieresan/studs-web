6fdb8f94d2b58c0ffa9a09d1a474953ab250be61
Aron Strandberg <strandberg.aron@gmail.com>
2017-11-26 22:01:31 +0100

    Redesign navbar

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -5,3 +5,5 @@ export const GET_USER_ERROR = 'app/App/GET_USER_ERROR'
 export const LOGIN_SUCCESS = 'app/App/LOGIN_SUCCESS'
 export const LOGIN_ERROR = 'app/App/LOGIN_ERROR'
 export const LOGOUT = 'app/App/LOGOUT'
+
+export const SET_NAVBAR_BACKGROUND = 'app/App/SET_NAVBAR_BACKGROUND'

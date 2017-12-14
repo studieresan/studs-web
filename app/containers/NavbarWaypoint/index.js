51ec092b4f77597b2a1cc5c93c3cd758140add8a
Aron Strandberg <strandberg.aron@gmail.com>
2017-12-14 15:52:40 +0100

    Add NavbarWaypoint

---
+++ app/containers/NavbarWaypoint/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import PropTypes from 'prop-types'
+import Waypoint from 'react-waypoint'
+import * as actions from 'containers/App/actions'
+
+class NavbarWaypoint extends Component {
+  constructor(props) {
+    super(props)
+    this.handleEnter = this.handleEnter.bind(this)
+    this.handleLeave = this.handleLeave.bind(this)
+  }
+
+  handleEnter() {
+    this.props.setNavbarBackground(true)
+  }
+
+  handleLeave() {
+    this.props.setNavbarBackground(false)
+  }
+
+  render() {
+    return (
+      <Waypoint
+        scrollableAncestor={window}
+        onEnter={this.handleEnter}
+        onLeave={this.handleLeave}>
+        <div>
+          { this.props.children }
+        </div>
+      </Waypoint>
+    )
+  }
+}
+
+NavbarWaypoint.propTypes = {
+  children: PropTypes.node.isRequired,
+  setNavbarBackground: PropTypes.func.isRequired,
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actions, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(NavbarWaypoint)

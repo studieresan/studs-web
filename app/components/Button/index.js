ab1ca73042f212517e62dc49012025dafd99f83e
Aron Strandberg <strandberg.aron@gmail.com>
2017-12-14 13:35:03 +0100

    Add Button component

---
+++ app/components/Button/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import styles from './styles.css'
+
+function Button({ type, disabled, onClick, children }) {
+  const classes = classNames({
+    [styles[`${type}`]]: type && !disabled,
+    [styles.disabled]: disabled,
+  }, styles.button)
+  return (
+    <div className={styles.wrapper}>
+      <button className={classes} onClick={onClick}>
+        { children }
+      </button>
+    </div>
+  )
+}
+
+Button.propTypes = {
+  type: PropTypes.oneOf([
+    'bright',
+    'danger',
+  ]),
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  children: PropTypes.node,
+}
+
+export default Button

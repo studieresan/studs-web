d79d3d047e21cd5b57d99dc9cba6b9cb782533b7
Aron Strandberg <strandberg.aron@gmail.com>
2017-12-21 20:44:08 +0100

    Redesign member component

--- app/components/MemberHomePage/index.js
+++ app/components/MemberHomePage/index.js
@@ -1,23 +1,25 @@
-/**
-*
-* MemberHomePage
-*
-*/
-
 import React from 'react'
-
+import PropTypes from 'prop-types'
 import styles from './styles.css'
 
-function MemberHomePage(props) {
-  const { user } = props
+function MemberHomePage({ user }) {
+  const { firstName, lastName } = user
+  const pic = `${process.env.CDN_URL}/members/${firstName.toLowerCase()}-${lastName.toLowerCase()}.jpg`
   return (
-    <div className={styles.memberHomePage}>
-      <div>
-        <img src={user.picture} />
-        { user.firstName}
-      </div>
+    <div className={styles.member}>
+      <img width='20rem' height='auto' src={pic} />
+      <h5 className={styles.name}>
+        { user.firstName }
+      </h5>
     </div>
   )
+}
+
+MemberHomePage.propTypes = {
+  user: PropTypes.shape({
+    firstName: PropTypes.string.isRequired,
+    picture: PropTypes.string,
+  }).isRequired,
 }
 
 export default MemberHomePage

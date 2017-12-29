97e9773d2e9b26e1379f34a30fab362e1f352c05
Jesper Sandström <jespersandstrom@gmail.com>
2017-12-29 14:03:38 +0100

    NotFoundPage implemented

--- src/containers/NotFoundPage/index.js
+++ src/containers/NotFoundPage/index.js
@@ -1,25 +1,20 @@
-/**
- * NotFoundPage
- *
- * This is the page we show when the user visits a url that doesn't have a route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+import { Link } from 'react-router-dom'
 import messages from './messages'
+import styles from './style.css'
 
 export default class NotFound extends React.Component {
-
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div className={styles.center}>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+        <Link to='/'>
+          Take me home
+        </Link>
+      </div>
     )
   }
 }
--- src/containers/NotFoundPage/messages.js
+++ src/containers/NotFoundPage/messages.js
@@ -1,13 +1,8 @@
-/*
- * NotFoundPage Messages
- *
- * This contains all the text for the NotFoundPage component.
- */
 import { defineMessages } from 'react-intl'
 
 export default defineMessages({
   header: {
     id: 'app.components.NotFoundPage.header',
-    defaultMessage: 'This is NotFoundPage component !',
+    defaultMessage: 'Invalid Link. Page Not Found. :(',
   },
 })

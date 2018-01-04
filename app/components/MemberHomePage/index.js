a1a70279e62e150ce5d663443e2f3b777058abd0
Aron Strandberg <strandberg.aron@gmail.com>
2018-01-04 18:16:01 +0100

    Switch to MemberImage

--- app/components/MemberHomePage/index.js
+++ app/components/MemberHomePage/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import MemberImage from 'components/MemberImage'
 import styles from './styles.css'
 
 function MemberHomePage({ user }) {
   const { firstName, position, picture } = user
   return (
     <div className={styles.member}>
-      <img width='20rem' height='auto' src={picture} />
+      <MemberImage picture={picture} size='20rem' />
       <h3 className={styles.name}>
         { firstName }
       </h3>

9a4325ce466b657b6c8bf56d29b4241212d6675f
Aron Strandberg <strandberg.aron@gmail.com>
2018-01-04 17:29:03 +0100

    fix react-router-dom Link import

--- app/components/HomePage/HomePageFooter/index.js
+++ app/components/HomePage/HomePageFooter/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 // import { FormattedMessage } from 'react-intl'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 import Isvg from 'react-inlinesvg'
 import { Facebook, Instagram, Github } from 'react-feather'
 import MemberImage from 'components/MemberImage'

d8660f790e30731d1eb809eb558cbf1fca6452c8
Aron Strandberg <strandberg.aron@gmail.com>
2018-01-04 18:56:54 +0100

    Add hot reloading

--- app/index.js
+++ app/index.js
@@ -2,8 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { AppContainer } from 'react-hot-loader'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+const render = (Component) => {
+  ReactDOM.render((
+    <AppContainer>
+      <Component />
+    </AppContainer>
+  ), document.getElementById('root'))
+}
 
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App)
+  })
+}
+
+registerServiceWorker()

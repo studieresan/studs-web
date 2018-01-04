54afaab011ada876a34473531abc3dbc88e6d6f5
Aron Strandberg <strandberg.aron@gmail.com>
2018-01-04 20:21:09 +0100

    super wip public events page

--- app/components/PublicEventMenu/index.js
+++ app/components/PublicEventMenu/index.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 import styles from './styles.css'
 
-const PublicEventMenu = ({events}) => {
+const PublicEventMenu = ({ events }) => {
   return (
     <div className={styles.publicEventMenu}>
       <h2>Events</h2>
-      { events.map(e =>
-        <Link activeClass={styles.active}
-          key={e.id}
-          to={e.id}
-          smooth={true}
-          offset={-92}
-          duration={400}
-          spy={true}
-        >
-          {e.company.name}
-        </Link>
-      )}
+      { events.map(e => <p key={e.companyName}>{ e.companyName }</p>) }
     </div>
+  // <Link activeClass={styles.active}
+  //   key={e.id}
+  //   to={e.id}
+  //   smooth={true}
+  //   offset={-92}
+  //   duration={400}
+  //   spy={true}
+  // >
+  // </Link>
   )
 }
 

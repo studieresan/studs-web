7de37271733c07bf59c304f951ad4cfec87c0b63
Aron Strandberg <strandberg.aron@gmail.com>
2018-01-05 09:17:06 +0100

    styling wip

--- app/components/PublicEvent/index.js
+++ app/components/PublicEvent/index.js
@@ -7,21 +7,21 @@ import Markdown from 'react-markdown'
 
 const PublicEvent = ({ companyName, publicDescription, pictures }) => (
   <div name={event.id} className={styles.publicEvent}>
-    <div className={styles.information}>
-      <h1 className={styles.companyName}>
-        { companyName }
-      </h1>
-      <h2>
-        {/* TODO
-          moment(event.date).format('MMMM Do') } @ { event.company.address
-        */}
-      </h2>
+    <h1 className={styles.companyName}>
+      { companyName }
+    </h1>
+    <div className={styles.content}>
+      {/*<h2>*/}
+      {/* TODO
+        moment(event.date).format('MMMM Do') } @ { event.company.address
+      */}
+      {/*</h2>*/}
       <div className={styles.description}>
         <Markdown source={publicDescription} />
       </div>
-    </div>
-    <div className={styles.pictures}>
-      { pictures.map(pic => <img key={pic} src={pic} />) }
+      <div className={styles.pictures}>
+        { pictures.map(pic => <img key={pic} src={pic} />) }
+      </div>
     </div>
   </div>
 )

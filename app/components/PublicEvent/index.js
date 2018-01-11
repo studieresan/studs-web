85eb4e6bda4b3f06468fd61961174a6ea6dabef2
Aron Strandberg <strandberg.aron@gmail.com>
2018-01-11 20:34:30 +0100

    Public Events page styling

--- app/components/PublicEvent/index.js
+++ app/components/PublicEvent/index.js
@@ -2,32 +2,39 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './styles.css'
-// import moment from 'moment' TODO do this without adding a dependecy
 import Markdown from 'react-markdown'
 
-const PublicEvent = ({ companyName, publicDescription, pictures }) => (
-  <div name={event.id} className={styles.publicEvent}>
-    <h1 className={styles.companyName}>
-      { companyName }
-    </h1>
-    <div className={styles.content}>
-      {/*<h2>*/}
-      {/* TODO
-        moment(event.date).format('MMMM Do') } @ { event.company.address
-      */}
-      {/*</h2>*/}
-      <div className={styles.description}>
-        <Markdown source={publicDescription} />
+const PublicEvent = ({ companyName, date, adress, publicDescription, pictures }) => {
+  return (
+    <div name={companyName} className={styles.publicEvent}>
+      <div className={styles.text}>
+        <div className={styles.info}>
+          <h1 className={styles.companyName}>
+            { companyName }
+          </h1>
+          <h5 className={styles.date}>
+            { date }
+          </h5>
+          <h5 className={styles.adress}>
+            { adress }
+          </h5>
+        </div>
+        <div className={styles.description}>
+          <Markdown source={publicDescription} />
+        </div>
       </div>
       <div className={styles.pictures}>
-        { pictures.map(pic => <img key={pic} src={pic} />) }
+        { pictures.map(pic => <img key={pic} src={pic} /> )}
       </div>
+      <div className={styles.divider} />
     </div>
-  </div>
-)
+  )
+}
 
 PublicEvent.propTypes = {
   companyName: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  adress: PropTypes.string.isRequired,
   publicDescription: PropTypes.string.isRequired,
   pictures: PropTypes.arrayOf(PropTypes.string).isRequired,
 }

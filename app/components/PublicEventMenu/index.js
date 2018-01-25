6e365633a49fe1ef8c1d299cd4dbdb19b49f6e25
Aron Strandberg <strandberg.aron@gmail.com>
2018-01-25 16:37:51 +0100

    Refactor Public Event Menu links

--- app/components/PublicEventMenu/index.js
+++ app/components/PublicEventMenu/index.js
@@ -9,26 +9,34 @@ const PublicEventMenu = ({ events }) => {
     <div className={styles.publicEventMenu}>
       <h2>Events</h2>
       <div className={styles.links}>
-        { events.map(e => (
-          <Link
-            activeClass={styles.active}
-            key={e.companyName}
-            to={e.companyName}
-            smooth={true}
-            offset={-92}
-            duration={400}
-            spy={true}
-          >
-            {e.companyName}
-          </Link>
-        ))}
+        { events.map(e => <PublicEventMenuLink key={e.companyName} company={e.companyName} />) }
       </div>
     </div>
   )
 }
 
+const PublicEventMenuLink = ({ company }) => {
+  return (
+    <Link
+      activeClass={styles.active}
+      key={company}
+      to={company}
+      smooth={true}
+      offset={-92}
+      duration={400}
+      spy={true}
+    >
+      {company}
+    </Link>
+  )
+}
+
 PublicEventMenu.propTypes = {
   events: PropTypes.array.isRequired,
+}
+
+PublicEventMenuLink.propTypes = {
+  company: PropTypes.string,
 }
 
 export default PublicEventMenu

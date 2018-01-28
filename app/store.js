016bf88895f2313b27f5d2b06dd9d0f10ce32619
Aron Strandberg <aron@aronstrandberg.com>
2018-01-28 11:40:57 +0100

    fix hot reloading reducers (#63)

--- app/store.js
+++ app/store.js
@@ -34,6 +34,14 @@ export default function configureStore(initialState = {}, history) {
     compose(...enhancers)
   )
 
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('./reducers', () => {
+      const nextRootReducer = require('./reducers/index')
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+
   return store
 }
 

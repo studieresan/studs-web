37b2afaace9c27c9f1f66d9ef719728c11fe0255
Michel Tabari <tabari@kth.se>
2018-09-05 08:14:26 +0200

    Remove return statement

--- app/components/RecruitmentRole/index.js
+++ app/components/RecruitmentRole/index.js
@@ -3,14 +3,11 @@ import PropTypes from 'prop-types'
 import StudsRecruitmentRoleModel from 'models/StudsRecruitmentRoleModel'
 import styles from './styles.css'
 
-const RecruitmentRole = ({ role }) => {
-  return (
-    <div className={styles.roleContainer}>
-      <h3 className={styles.title}>{role.title}</h3>
-      <p className={styles.description}><span className={styles.boldIntro}>{role.shortDescription}</span>{role.description}</p>
-    </div>
-  )
-}
+const RecruitmentRole = ({ role }) =>
+  <div className={styles.roleContainer}>
+    <h3 className={styles.title}>{role.title}</h3>
+    <p className={styles.description}><span className={styles.boldIntro}>{role.shortDescription}</span>{role.description}</p>
+  </div>
 
 RecruitmentRole.propTypes = {
   role: PropTypes.PropTypes.instanceOf(StudsRecruitmentRoleModel).isRequired,

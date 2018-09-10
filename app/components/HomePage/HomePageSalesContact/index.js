3f6a1c391cc5b620554abde2bba7cb6cb9a7ba91
Michel Tabari <tabari@kth.se>
2018-09-10 22:07:04 +0200

    Tweaks

--- app/components/HomePage/HomePageSalesContact/index.js
+++ app/components/HomePage/HomePageSalesContact/index.js
@@ -5,18 +5,23 @@ import styles from './styles.css'
 import messages from './messages'
 import Nils from 'static/img/people/nils.png'
 
+const CONTACT_NAME = 'Nils Streijffert'
+const CONTACT_PHONE_NUMBER = '+4670-1440857'
+const CONTACT_EMAIL = 'studs-salj@d.kth.se'
+
 const HomePageSalesContact = () =>
   <div className={styles.contact}>
     <Section {...messages.contact} />
     <div className={styles.contactInfo}>
       <img src={Nils}/>
       <div>
-        <b>Nils Streijffert</b>
+        <b>{CONTACT_NAME}</b>
         <br/>
-        <FormattedMessage {...messages.contact.phone} />{': +4670-144 08 57'}
+        <FormattedMessage {...messages.contact.phone} />{': '}
+        <a href={`tel:${CONTACT_PHONE_NUMBER}`}>{`${CONTACT_PHONE_NUMBER}`}</a>
         <br/>
         <span>{'Email: '}</span>
-        <a href="mailto:studs-salj@d.kth.se">{'studs-salj@d.kth.se'}</a>
+        <a href={`mailto:${CONTACT_EMAIL}`}>{CONTACT_EMAIL}</a>
       </div>
     </div>
   </div>

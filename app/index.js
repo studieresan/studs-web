b81d8ca399e0ba7124dd721d6fc6a7dc7f5fd5f4
Sven Anderzén <svenanderzen@users.noreply.github.com>
2018-10-20 23:48:57 +0200

    Adding sentry browser logging

--- app/index.js
+++ app/index.js
@@ -5,6 +5,11 @@ import App from './App'
 import { AppContainer } from 'react-hot-loader'
 import ReactGA from 'react-ga'
 import { unregister } from './registerServiceWorker'
+import * as Sentry from '@sentry/browser'
+
+Sentry.init({
+  dsn: 'https://544ec1664d084abea6f10bf97334a80b@sentry.io/1298596',
+})
 
 if (process.env.NPM_CONFIG_PRODUCTION === 'production') {
   ReactGA.initialize('UA-125271813-1')

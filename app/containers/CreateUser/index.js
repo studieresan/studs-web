364086be7db3bc7c8c57750a93388ea0bdd48b28
Willy Liu <arkazain@gmail.com>
2018-10-22 15:12:19 +0200

    Implement signup

--- app/containers/CreateUser/index.js
+++ app/containers/CreateUser/index.js
@@ -1,14 +1,33 @@
 import React from 'react'
+import { createUser } from 'api'
 import Button from 'components/Button'
 import styles from './styles.css'
+import formToObject from './formUtils'
 
 class CreateUser extends React.Component {
+  // If we use babel-eslint as parser, we can remove this constructor
+  // and use an arrow function for handleSubmit instead
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const formData = formToObject(event.target.elements)
+    console.log(formData)
+    createUser(formData)
+      .then(res => {
+        console.log(res)
+      })
+  }
+
   render() {
     return (
       <div className={styles.createUser}>
         <div className={styles.content}>
           <h1>Create user</h1>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <label>
               Email:
               <input
@@ -16,6 +35,7 @@ class CreateUser extends React.Component {
                 placeholder="some@email.com"
                 name="email"
                 id="email"
+                required
               />
             </label>
 
@@ -26,6 +46,7 @@ class CreateUser extends React.Component {
                 placeholder="First name"
                 name="firstName"
                 id="firstName"
+                required
               />
             </label>
 
@@ -36,37 +57,29 @@ class CreateUser extends React.Component {
                 placeholder="Last name"
                 name="lastName"
                 id="lastName"
-              />
-            </label>
-
-            <label>
-              Password:
-              <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                id="password"
-              />
-            </label>
-
-            <label>
-              Confirm password:
-              <input
-                type="password"
-                placeholder="confirmPassword"
-                name="confirmPassword"
-                id="confirmPassword"
+                required
               />
             </label>
 
             <div className={styles.formLabel}>
               Member type:
               <div className={styles.radioButtonGroup}>
-                <input type="radio" name="memberType" id="studs_member"/>
+                <input
+                  type="radio"
+                  name="memberType"
+                  id="studs_member"
+                  value="studs_member"
+                  defaultChecked
+                />
                 <label htmlFor="studs_member">Studs member</label>
                 <br/>
 
-                <input type="radio" name="memberType" id="company_member"/>
+                <input
+                  type="radio"
+                  name="memberType"
+                  id="company_member"
+                  value="company_member"
+                />
                 <label htmlFor="company_member">Company member</label>
               </div>
             </div>

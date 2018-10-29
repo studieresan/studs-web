735499feda613cc7993e45b08e28a8a7828f5db8
Michel Tabari <tabari@kth.se>
2018-10-29 20:04:47 +0100

    Update year

--- app/components/PublicEventMenu/index.js
+++ app/components/PublicEventMenu/index.js
@@ -4,12 +4,12 @@ import { Link } from 'react-scroll'
 
 import styles from './styles.css'
 
-const CURRENT_STUDS_YEAR = 2018
+const CURRENT_STUDS_YEAR = 2019
 
 const PublicEventMenu = ({ events }) => {
   return (
     <div className={styles.publicEventMenu}>
-      <h2>Events {CURRENT_STUDS_YEAR}</h2>
+      <h2>Events Studs {CURRENT_STUDS_YEAR}</h2>
       <div className={styles.links}>
         { events.map(e => <PublicEventMenuLink key={e.companyName} company={e.companyName} />) }
       </div>

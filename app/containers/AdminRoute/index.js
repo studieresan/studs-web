3bfb8144ab6156c4b9faa8e90d918ad2e1c01b69
Willy Liu <arkazain@gmail.com>
2018-11-05 12:56:51 +0100

    Update AdminRoute

--- app/containers/AdminRoute/index.js
+++ app/containers/AdminRoute/index.js
@@ -4,27 +4,52 @@ import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
 import { List, Map } from 'immutable'
 
-type Props = {
-  component: React.Node,
+type State = {|
   isAdmin: boolean,
-}
+  loggedIn: boolean,
+  hasFetchedLoggedInUser: boolean,
+|}
+type Props = {|
+  ...State,
+  component: React.ComponentType<any>,
+|}
 
-function AdminRoute({ component: Component, isAdmin, ...rest }: Props) {
+function AdminRoute({
+  component: Component,
+  hasFetchedLoggedInUser,
+  loggedIn,
+  isAdmin,
+  ...rest
+}: Props) {
+  if (!hasFetchedLoggedInUser && loggedIn) {
+    // The user is logged in, but the user object hasn't loaded yet
+    return null
+  }
+
+  // User is logged in, only show Component if they have admin permissions
   return (
     <Route
       {...rest}
-      render={() => (isAdmin ? Component : <Redirect to="/" />)}
+      render={props =>
+        isAdmin ? <Component {...props} /> : <Redirect to="/" />
+      }
     />
   )
 }
 
-function mapStateToProps(state: Map<string, any>) {
+function mapStateToProps(state: Map<string, any>): State {
   const userPermissions: List<string> =
-    state.getIn(['user', 'user', 'permissions']) || List()
+    state.getIn(['global', 'user', 'permissions']) || List()
 
   return {
-    isAdmin: userPermissions.includes('admin'),
+    isAdmin: userPermissions.includes('admin_permission'),
+    loggedIn: state.getIn(['global', 'loggedIn']) || false,
+    hasFetchedLoggedInUser:
+      state.getIn(['global', 'hasFetchedLoggedInUser']) || false,
   }
 }
 
-export default connect(mapStateToProps, null)(AdminRoute)
+export default connect(
+  mapStateToProps,
+  null,
+)(AdminRoute)

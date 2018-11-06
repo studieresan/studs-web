290300648cb764f4c9b4cf590591a10d06f31f94
Michel Tabari <tabari@kth.se>
2018-11-06 12:49:15 +0100

    Initial work on detail page

--- app/components/EventDetail/index.js
+++ app/components/EventDetail/index.js
@@ -1,40 +1,24 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import { FormattedMessage } from 'react-intl'
-import { Link } from 'react-router-dom'
+import {connect} from 'react-redux'
+import {FormattedMessage} from 'react-intl'
+import {Link} from 'react-router-dom'
 import messages from './messages'
 import Markdown from 'react-markdown'
 import Button from 'components/Button'
-import { Chart } from 'react-google-charts' // TODO replace with chart.js
-import { hasEventPermission } from 'users'
+import {Chart} from 'react-google-charts' // TODO replace with chart.js
+import {hasEventPermission} from 'users'
 
 import styles from './styles.css'
 import A from 'components/A'
 import IndicatorIcon from 'components/IndicatorIcon'
 
 class EventDetail extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  componentDidMount() {
-    // TODO
-    // this.getMissingForms()
-  }
-
-  componentDidUpdate() {
-    // TODO
-    // if (prevProps.id !== this.props.id) {
-    //   this.getMissingForms()
-    // }
-  }
-
   render() {
-    const { event, user, onRemoveEvent } = this.props
+    const {event, user, onRemoveEvent} = this.props
     if (!event) {
       return null
     }
-
     let after, before
     if (event.after && event.before) {
       const userList = person => (
@@ -49,124 +33,125 @@ class EventDetail extends Component {
       <div className={styles.eventDetail}>
         <div className={styles.head}>
           <h2>
-            <FormattedMessage {...messages.event} />: {event.companyName} -{' '}
-            {event.date && event.date.toString()}
+            <FormattedMessage {...messages.event} />
+            : {event.companyName} - {event.date && event.date.toString()}
           </h2>
-          {hasEventPermission(user) && (
-            <div className={styles.buttonRow}>
-              <Link to={`/events/${event.id}/edit`}>
-                <Button color='bright'>Edit</Button>
-              </Link>
-              <Button color='danger' onClick={() => onRemoveEvent(event.id)}>
-                Delete
+          {hasEventPermission(user) &&
+          <div className={styles.buttonRow}>
+            <Link to={`/events/${event.id}/edit`}>
+              <Button
+                color='bright'>
+                Edit
               </Button>
-            </div>
-          )}
+            </Link>
+            <Button
+              color='danger'
+              onClick={() => onRemoveEvent(event.id)}>
+              Delete
+            </Button>
+          </div>
+          }
         </div>
         <div className={styles.info}>
           <div>
             <h4>Company</h4>
             <div>{event.companyName}</div>
           </div>
-          {user && user.memberType === 'studs_member' && (
+          <div>
+            <h4>Location</h4>
+            <div>{event.location}</div>
+          </div>
+          {user && user.type === 'studs_member' &&
+          <div>
+            <h4>Surveys</h4>
             <div>
-              <h4>Surveys</h4>
-              <div>
-                <IndicatorIcon ok={event.beforeSurveyReplied} />
-                <A target='_blank' href={event.beforeSurvey}>
-                  <FormattedMessage {...messages.before} />
-                </A>
-              </div>
-              <div>
-                <IndicatorIcon ok={event.afterSurveyReplied} />
-                <A target='_blank' href={event.afterSurvey}>
-                  <FormattedMessage {...messages.after} />
-                </A>
-              </div>
+              <IndicatorIcon ok={event.beforeSurveyReplied}/>
+              <A target='_blank' href={event.beforeSurvey}>
+                <FormattedMessage {...messages.before} />
+              </A>
             </div>
-          )}
+            <div>
+              <IndicatorIcon ok={event.afterSurveyReplied}/>
+              <A target='_blank' href={event.afterSurvey}>
+                <FormattedMessage {...messages.after} />
+              </A>
+            </div>
+          </div>
+          }
         </div>
-        {event.description && (
+        {event.publicDescription &&
+        <div>
+          <hr/>
+          <h2><FormattedMessage {...messages.description} /></h2>
+          <Markdown source={event.publicDescription}/>
+        </div>
+        }
+        {user.type === 'studs_member' && event.privateDescription &&
+        <div>
+          <hr/>
+          <h2><FormattedMessage {...messages.privateDescription} /></h2>
+          <Markdown source={event.privateDescription}/>
+        </div>
+        }
+        {event.formData && event.feedbackText &&
+        <div>
+          <hr/>
+          <h2><FormattedMessage {...messages.feedback} /></h2>
+          <Markdown source={event.feedbackText}/>
+        </div>
+        }
+        {event.formData &&
+        <div className={styles.charts}>
+          <Chart
+            chartType='PieChart'
+            data={event.formData.beforeInterest}
+            options={{
+              title: 'Interest in the company before the event',
+            }}
+            graph_id='beforeInterest'
+            width='100%'/>
+          <Chart
+            chartType='PieChart'
+            data={event.formData.afterInterest}
+            options={{
+              title: 'Interest in the company after the event',
+            }}
+            graph_id='afterInterest'
+            width='100%'/>
+          <Chart
+            chartType='PieChart'
+            data={event.formData.knowDoes}
+            options={{
+              title: 'Do you know what the company does (before the event)?',
+            }}
+            graph_id='knowDoes'
+            width='100%'/>
+          <Chart
+            chartType='PieChart'
+            data={event.formData.qualified}
+            options={{
+              title: 'Do you feel like you are qualified to work at' +
+                'this company?',
+            }}
+            graph_id='qualified'
+            width='100%'/>
+        </div>
+        }
+        {hasEventPermission(user)
+        && (before && before.length || after && after.length) ? (
           <div>
-            <hr />
-            <h2>
-              <FormattedMessage {...messages.description} />
-            </h2>
-            <Markdown source={event.description} />
-          </div>
-        )}
-        {event.formData && event.feedbackText && (
-          <div>
-            <hr />
-            <h2>
-              <FormattedMessage {...messages.feedback} />
-            </h2>
-            <Markdown source={event.feedbackText} />
-          </div>
-        )}
-        {event.formData && (
-          <div className={styles.charts}>
-            <Chart
-              chartType='PieChart'
-              data={event.formData.beforeInterest}
-              options={{
-                title: 'Interest in the company before the event',
-              }}
-              graph_id='beforeInterest'
-              width='100%'
-            />
-            <Chart
-              chartType='PieChart'
-              data={event.formData.afterInterest}
-              options={{
-                title: 'Interest in the company after the event',
-              }}
-              graph_id='afterInterest'
-              width='100%'
-            />
-            <Chart
-              chartType='PieChart'
-              data={event.formData.knowDoes}
-              options={{
-                title: 'Do you know what the company does (before the event)?',
-              }}
-              graph_id='knowDoes'
-              width='100%'
-            />
-            <Chart
-              chartType='PieChart'
-              data={event.formData.qualified}
-              options={{
-                title:
-                  'Do you feel like you are qualified to work at' +
-                  'this company?',
-              }}
-              graph_id='qualified'
-              width='100%'
-            />
-          </div>
-        )}
-        {hasEventPermission(user) &&
-        ((before && before.length) || (after && after.length)) ? (
-          <div>
-            <hr />
-            <h2>
-              <FormattedMessage {...messages.missing} />
-            </h2>
+            <hr/>
+            <h2><FormattedMessage {...messages.missing} /></h2>
             <div className={styles.missing}>
               <div>
                 {before && before.length ? (
                   <div>
                     <div className={styles.missingHead}>Before</div>
-                    {!event.remindedBefore && (
-                      <button
-                        name='before'
-                        onClick={this.handleRemindClick}
-                        className='btn-default'
-                      >
-                        Remind on slack
-                      </button>
-                    )}
+                    {!event.remindedBefore &&
+                    <button name='before'onClick={this.handleRemindClick} className='btn-default'>
+                      Remind on slack
+                    </button>
+                    }
                     <ul>{before}</ul>
                   </div>
                 ) : null}
@@ -175,15 +160,11 @@ class EventDetail extends Component {
                 {after && after.length ? (
                   <div>
                     <div className={styles.missingHead}>After</div>
-                    {!event.remindedAfter && (
-                      <button
-                        name='after'
-                        onClick={this.handleRemindClick}
-                        className='btn-default'
-                      >
-                        Remind on slack
-                      </button>
-                    )}
+                    {!event.remindedAfter &&
+                    <button name='after' onClick={this.handleRemindClick} className='btn-default'>
+                      Remind on slack
+                    </button>
+                    }
                     <ul>{after}</ul>
                   </div>
                 ) : null}
@@ -196,6 +177,12 @@ class EventDetail extends Component {
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    user: state.getIn(['global', 'user']).toJS(),
+  }
+}
+
 EventDetail.propTypes = {
   id: PropTypes.string.isRequired,
   event: PropTypes.object.isRequired,
@@ -203,4 +190,4 @@ EventDetail.propTypes = {
   onRemoveEvent: PropTypes.func.isRequired,
 }
 
-export default EventDetail
+export default connect(mapStateToProps)(EventDetail)

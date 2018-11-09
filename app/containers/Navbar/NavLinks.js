9f0408ca92db0b3736cf090e8cbb549752af640f
Willy Liu <arkazain@gmail.com>
2018-11-09 11:44:34 +0100

    Implement new navbar

---
+++ app/containers/Navbar/NavLinks.js
@@ -0,0 +1,80 @@
+// @flow
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FormattedMessage } from 'react-intl'
+import messages from './messages'
+import styles from './styles.css'
+import LocaleToggle from '../LocaleToggle'
+
+type Props = {
+  loggedIn: boolean,
+  user: Object,
+}
+
+/**
+ * Component for the actual links in the navbar.
+ * Extracted into its own component since it is kind of big.
+ */
+function NavLinks({ loggedIn, user }: Props) {
+  let userLinks
+  if (loggedIn) {
+    userLinks = (
+      <React.Fragment>
+        <li>
+          <Link to="/user">{user.firstName || 'Profile'}</Link>
+        </li>
+        <li>
+          <Link to="/members">
+            <FormattedMessage {...messages.studsmembers} />
+          </Link>
+        </li>
+        <li>
+          <Link to="/trip">
+            <FormattedMessage {...messages.trip} />
+          </Link>
+        </li>
+        <li>
+          <Link to="/logout">
+            <FormattedMessage {...messages.logout} />
+          </Link>
+        </li>
+      </React.Fragment>
+    )
+  } else {
+    userLinks = (
+      <li>
+        <Link to="/login">
+          <FormattedMessage {...messages.login} />
+        </Link>
+      </li>
+    )
+  }
+
+  return (
+    <div className={styles.navLinks}>
+      <ul className={styles.navbarMenu}>
+        <li>
+          <Link to="/about">
+            <FormattedMessage {...messages.about} />
+          </Link>
+        </li>
+        <li>
+          <Link to="/events">
+            <FormattedMessage {...messages.internalevents} />
+          </Link>
+        </li>
+        <li>
+          <Link to="/student">Student</Link>
+        </li>
+      </ul>
+      <ul className={styles.navbarUserMenu}>
+        <li>
+          <LocaleToggle />
+        </li>
+        {userLinks}
+      </ul>
+    </div>
+  )
+}
+
+export default NavLinks
--- app/containers/Navbar/index.js
+++ app/containers/Navbar/index.js
@@ -1,166 +1,19 @@
+// @flow
 import React from 'react'
-import PropTypes from 'prop-types'
-import { FormattedMessage } from 'react-intl'
-import { bindActionCreators } from 'redux'
-import classNames from 'classnames'
-import styles from './styles.css'
-import LocaleToggle from '../LocaleToggle'
-import Black from 'static/img/logo/Black.svg'
-import White from 'static/img/logo/white.svg'
-import MenuIcon from './icon_menu.svg'
-import BlackMenuIcon from './icon_menu_black.svg'
-import CloseIcon from './icon_close.svg'
-import BlackCloseIcon from './icon_close_black.svg'
+import { Switch, Route } from 'react-router-dom'
+import HeroNav from './HeroNav'
 
-import { withRouter, Link } from 'react-router-dom'
-import messages from './messages'
-import { connect } from 'react-redux'
-import * as actions from '../User/actions'
-
-export class Navbar extends React.Component {
-  constructor(props) {
-    super(props)
-    this.menu = this.menu.bind(this)
-    this.handleMenuClick = this.handleMenuClick.bind(this)
-    this.state = {
-      collapsed: true,
-    }
-  }
-
-  handleMenuClick() {
-    this.setState({ collapsed: !this.state.collapsed })
-  }
-
-  componentWillMount() {
-    this.setState({ collapsed: true })
-  }
-
-  menu() {
-    if (this.props.loggedIn) {
-      const user = this.props.user
-      return (
-        <ul className={styles.navbarMenu}>
-          <li>
-            <Link to='/logout'>
-              <FormattedMessage {...messages.logout} />
-            </Link>
-          </li>
-          <li>
-            <Link to='/user'>{user.firstName || 'Profile'}</Link>
-          </li>
-          <li>
-            <Link to='/members'>
-              <FormattedMessage {...messages.studsmembers} />
-            </Link>
-          </li>
-          <li>
-            <Link to='/events'>
-              <FormattedMessage {...messages.internalevents} />
-            </Link>
-          </li>
-          {/* TODO <li>
-              <Link to="/events/public">
-                <FormattedMessage {...messages.events} />
-              </Link>
-            </li> */}
-          <li>
-            <Link to='/trip'>
-              <FormattedMessage {...messages.trip} />
-            </Link>
-          </li>
-        </ul>
-      )
-    } else {
-      return (
-        <ul className={styles.navbarMenu}>
-          <li>
-            <Link to='/about'>
-              <FormattedMessage {...messages.about} />
-            </Link>
-          </li>
-          <li>
-            <Link to='/events/public'>
-              <FormattedMessage {...messages.events} />
-            </Link>
-          </li>
-          <li>
-            <Link to='/events/2018'>
-              <FormattedMessage {...messages.pastEvents} />
-            </Link>
-          </li>
-          <li>
-            <Link to='/student'>Student</Link>
-          </li>
-          <li>
-            <Link to='/login'>
-              <FormattedMessage {...messages.login} />
-            </Link>
-          </li>
-        </ul>
-      )
-    }
-  }
-
-  render() {
-    let collapsed = styles.collapse
-    collapsed += this.state.collapsed ? ' ' + styles.collapsed : ''
-
-    const bg = true // this.props.displayNavbarBackground
-    const navbarClasses = classNames(styles.navbar, {
-      [styles.background]: bg,
-    })
-    const Logo = bg ? Black : White
-    return (
-      <div className={navbarClasses}>
-        <div className={styles.control}>
-          <Link to='/'>
-            <img src={Logo} height={24} />
-          </Link>
-          <img
-            src={
-              this.state.collapsed
-                ? bg
-                  ? BlackMenuIcon
-                  : MenuIcon
-                : bg
-                ? BlackCloseIcon
-                : CloseIcon
-            }
-            height={24}
-            onClick={this.handleMenuClick}
-          />
-        </div>
-        <div className={collapsed}>
-          {this.menu()}
-          <LocaleToggle />
-        </div>
-      </div>
-    )
-  }
+/**
+ * Use a different variation of the navbar depending on which
+ * page you are in currently.
+ */
+function Navbar() {
+  return (
+    <Switch>
+      <Route exact path='/' render={props => <HeroNav {...props} invert />} />
+      <Route component={HeroNav} />
+    </Switch>
+  )
 }
 
-Navbar.propTypes = {
-  user: PropTypes.object,
-  displayNavbarBackground: PropTypes.bool,
-  loggedIn: PropTypes.bool.isRequired,
-}
-
-function mapStateToProps(state) {
-  const immutableUser = state.getIn(['global', 'user'])
-  return {
-    user: immutableUser ? immutableUser.toJS() : {},
-    loggedIn: state.getIn(['global', 'loggedIn']),
-    displayNavbarBackground: state.getIn(['global', 'displayNavbarBackground']),
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(actions, dispatch)
-}
-
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Navbar)
-)
+export default Navbar

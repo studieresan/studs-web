b746a658189c073f968866c06be83048d8d80a34
Moa Nyman <moanyman94@gmail.com>
2019-04-06 17:33:39 +0200

    Simplify component

--- app/components/IndicatorIcon/index.js
+++ app/components/IndicatorIcon/index.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classNames from 'classnames/bind'
-
 import styles from './styles.css'
 
-const cx = classNames.bind(styles)
-
 const IndicatorIcon = ({ ok }) => {
-  const className = cx({
-    indicatorIcon: true,
-    ok: ok,
-  })
-  return <span className={className}>&#9679;</span>
+  return (
+    <span className={styles.indicatorIcon + (ok ? ' ' + styles.ok : '')}>
+      &#9679;
+    </span>
+  )
 }
 
 IndicatorIcon.propTypes = {

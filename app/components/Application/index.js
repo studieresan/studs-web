366bebf5c139817f9759ad845a63cc12955dbcbb
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-08-15 18:14:37 +0200

    Add application form

--- app/components/Application/index.js
+++ app/components/Application/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
+import Iframe from 'react-iframe'
+import styles from './styles.css'
 
 export class ApplicationComponent extends Component {
   render() {
-    return <div />
+    return (
+      <div className={styles.form_wrapper}>
+        <Iframe
+          url='https://docs.google.com/forms/d/e/1FAIpQLSeg5yrJPMYsHWZiAdq-ERhUhDTce8IKP4T7TupvD4U2v9vyZQ/viewform?embedded=true'
+          width='100%'
+          height='100%'
+        />
+      </div>
+    )
   }
 }

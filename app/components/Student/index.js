0bf504b85c1d949160097ec4b21e5f25c78643fa
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-08-19 19:41:41 +0200

    Update layout and text

--- app/components/Student/index.js
+++ app/components/Student/index.js
@@ -34,7 +34,7 @@ export class StudentComponent extends Component {
             projektet ska lyckas. Mycket av arbetet i Studs sker grupperna
             emellan och det är därför viktigt att du som gruppmedlem är
             samarbetsvillig och förstår att vi alla arbetar mot ett gemensamt
-            mål. Studs kommer inte att bara ge dig stora kunskaper inom allt vad
+            mål. Studs kommer inte att bara ge dig goda kunskaper inom allt vad
             projektarbete innebär, utan du kommer också att ha ett otroligt
             roligt år och knyta värdefulla relationer med näringslivet och inom
             gruppen!
@@ -53,8 +53,7 @@ export class StudentComponent extends Component {
                 Ekonomigruppen är liten grupp som jobbar nära varandra och
                 projektledarna med att skapa projektets budget och se till att
                 den efterföljs. Gruppen sköter även all bokföring och
-                tillgodoser de andra gruppernas ekonomiska behov. Arbetet är
-                kontinuerligt under hela året.
+                tillgodoser de andra gruppernas ekonomiska behov.
                 <br />
                 Har du funderingar? Kontakta Klara!
               </p>
@@ -72,7 +71,7 @@ export class StudentComponent extends Component {
             </div>
             <div className={styles.info}>
               <p>
-                Eventgruppen ansvarar för planerandet och genomförandet för alla
+                Eventgruppen ansvarar för planerandet och genomförandet av alla
                 företagsevent och arbetar tillsammans med företagen för att
                 skapa evenemang som är relevanta, intressanta och roliga både
                 för projektmedlemmarna och företagen. Gruppen hjälper företagen
@@ -95,13 +94,14 @@ export class StudentComponent extends Component {
             </div>
             <div className={styles.info}>
               <p>
-                Informationsgruppen består av fotografer, redaktörer och en art
-                director som tillsammans skapar allt innehåll och den grafiska
-                profil som syns i våra olika kanaler samt rapporten.
+                Informationsgruppen består av fotografer, redaktörer och två art
+                directors som tillsammans skapar allt innehåll och den grafiska
+                profil som syns i våra olika kanaler samt i rapporten.
                 Fotograferna tar porträttbilder av projektgruppen samt
                 fotograferar alla event och resan. Redaktörerna producerar all
-                text i sociala medier och rapporten och art directorn skapar
-                layouten för rapporten.
+                text i sociala medier och rapporten. Gruppens art directors
+                skapar den grafiska profilen och är ansvariga webb-, app- och
+                UX-design. De ansvar också för rapportens layout.
                 <br />
                 Har du funderingar? Kontakta Samuel!
               </p>
@@ -120,12 +120,10 @@ export class StudentComponent extends Component {
             <div className={styles.info}>
               <p>
                 IT-gruppen ansvarar för Studs webbsida och de interna
-                mobilapplikationerna. De programmeringsspråk som används är
-                JavaScript (React, Node), Kotlin (Android) och Swift (iOS). Du
-                bör ha ett intresse för programmering och mjukvaruutveckling och
-                ett sug efter att lära dig nya saker. Det är ett plus om du har
-                tidigare erfarenhet, men det är inget krav. Arbetsbelastningen
-                är kontinuerlig under året.
+                mobilapplikationerna som finns för både Android och iOS. Du bör
+                ha ett intresse för programmering och mjukvaruutveckling samt
+                viljan att lära dig nya saker. Det är ett plus om du har
+                tidigare erfarenhet, men det är inget krav.
                 <br />
                 Har du funderingar? Kontakta Marko!
               </p>
@@ -144,13 +142,13 @@ export class StudentComponent extends Component {
             <div className={styles.info}>
               <p>
                 Resegruppen ansvarar för all planering och genomförandet av
-                resan som avslutar projektet - allt från resrutt och flyg till
+                resan som avslutar projektet, allt från resrutt och flyg till
                 koordinering av dagsaktiviteter. Arbetet är både kreativt och
-                strukturerat, då resegruppen utöver planering och logistik också
+                strukturerat då resegruppen utöver planering och logistik också
                 är ansvarig för att ta fram inspiration och förslag på resmål.
-                Som medlem i resegruppen leder du hela gruppen under resan och
-                ser till att alla har det bra och hanterar oförutsägbara
-                situationer. Merparten av arbetet sker under hösten.
+                Som medlem i resegruppen leder du hela projektgruppen under
+                resan, ser till att alla har det bra och hanterar oförutsägbara
+                situationer.
                 <br />
                 Har du funderingar? Kontakta Carl!
               </p>
@@ -169,13 +167,11 @@ export class StudentComponent extends Component {
             <div className={styles.info}>
               <p>
                 Medlemmarna i säljgruppen skapar den första kontakten mellan
-                Studs och företag och säljer in event hos företagen. Det är
-                viktigt att du som medlem i säljgruppen är utåtriktad och
-                social, och inte tappar glöden om du får ett ”Nej tack” från ett
-                företag. Säljgruppen jobbar nära flera andra grupper,
-                framförallt eventgruppen som tar över efter sälj när ett event
-                har blivit sålt. För säljgruppen är det som mest arbete under
-                hösten, då majoriteten av vårens event säljs.
+                Studs och företag och säljer in event. Det är viktigt att du som
+                medlem i säljgruppen är utåtriktad och social, och inte tappar
+                glöden om du får ett nej. Säljgruppen jobbar nära flera andra
+                grupper, framförallt eventgruppen som tar över när ett event har
+                blivit bokat.
                 <br />
                 Har du funderingar? Kontakta Cristian!
               </p>

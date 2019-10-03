781bfdf724c4c9b2786a713d7da7166a57c9dd9a
Anton Stagge <antonstagge95@gmail.com>
2019-10-03 13:24:54 +0200

    Add redux value for sorting

--- app/containers/SalesTool/store/actions.js
+++ app/containers/SalesTool/store/actions.js
@@ -4,3 +4,8 @@ export const updateFilter = newFilter => ({
   type: UPDATE_FILTER,
   payload: newFilter,
 })
+
+export const updateSorting = newSorting => ({
+  type: UPDATE_FILTER,
+  payload: newSorting,
+})

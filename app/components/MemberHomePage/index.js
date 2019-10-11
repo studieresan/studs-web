29fc36d607db4e6655cc7037ff095066b2c70361
Anton Stagge <antonstagge95@gmail.com>
2019-10-11 16:44:43 +0200

    swap positon to userRole in last place

--- app/components/MemberHomePage/index.js
+++ app/components/MemberHomePage/index.js
@@ -2,15 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import MemberImage from 'components/MemberImage'
 import styles from './styles.css'
+import { prettyUserRole } from 'utils'
 
 function MemberHomePage({ user }) {
-  const { firstName, lastName, position, picture } = user
-  console.log('AOWIJDAOWIJ')
+  const { firstName, lastName, userRole, picture } = user
   return (
     <div className={styles.member}>
       <MemberImage picture={picture} size='20rem' />
       <h3 className={styles.name}>{firstName + ' ' + lastName}</h3>
-      <h5 className={styles.position}>{position}</h5>
+      <h5 className={styles.position}>{prettyUserRole(userRole)}</h5>
     </div>
   )
 }

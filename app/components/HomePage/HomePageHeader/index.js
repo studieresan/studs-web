5a9daeb05002bbb1df1b17cffda11283b33ccd90
Anton Stagge <antonstagge95@gmail.com>
2019-11-05 10:48:25 +0100

    Update homepage header

--- app/components/HomePage/HomePageHeader/index.js
+++ app/components/HomePage/HomePageHeader/index.js
@@ -3,13 +3,15 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import messages from './messages'
 import styles from './styles.css'
-import LogoPng from 'static/img/logo/studs20.png'
+import LogoPng from 'static/img_new/logo/logotype-white.png'
+import Background from 'static/img_new/home/background.png'
 import Button from 'components/Button'
 import { Link } from 'react-router-dom'
 
 function HomePageHeader() {
   return (
     <div className={styles.header}>
+      <img className={styles.background} src={Background} />
       <div className={styles.header_content}>
         <img className={styles.logoDesktop} src={LogoPng} />
         <img className={styles.logoMobile} src={LogoPng} />

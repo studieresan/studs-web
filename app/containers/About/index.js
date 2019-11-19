296ca03dba4d31d44252bd320dcd9ae9d1c4eb00
Anton Stagge <antonstagge95@gmail.com>
2019-11-19 12:06:15 +0100

    Show all studs members in about

--- app/containers/About/index.js
+++ app/containers/About/index.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
-// import { connect } from 'react-redux'
-// import { bindActionCreators } from 'redux'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { FormattedMessage } from 'react-intl'
-// import sortBy from 'lodash/sortBy'
-// import * as actions from 'containers/Members/actions'
-import MemberHomePage from 'components/MemberHomePage'
+import sortBy from 'lodash/sortBy'
+import * as actions from 'containers/Members/actions'
 import styles from './styles.css'
 import messages from './messages'
 import Footer from 'components/Footer'
-import Josefin from 'static/img_new/people/josefin.jpg'
-import Cristian from 'static/img_new/people/cristian.jpg'
-import Christine from 'static/img_new/people/christine.jpg'
-import Klara from 'static/img_new/people/klara.jpg'
-import Samuel from 'static/img_new/people/samuel.jpg'
-import Marko from 'static/img_new/people/marko.jpg'
-import Helena from 'static/img_new/people/helena.jpg'
-import Carl from 'static/img_new/people/carl.jpg'
 import MemberImage from 'components/MemberImage'
+import placeholder from 'static/img_new/profile-placeholder.png'
+import { prettyUserRole } from 'utils'
 import { StudentComponent } from 'components/Student/index'
-import Button from 'components/Button'
-import { Link } from 'react-router-dom'
+
+const StudsMemberInfo = user => (
+  <div className={styles.member}>
+    <MemberImage
+      picture={user.picture ? user.picture : placeholder}
+      size={200}
+      square
+      round
+    />
+    <h3>
+      {user.firstName} {user.lastName}
+    </h3>
+    <h5>{prettyUserRole(user.userRole)}</h5>
+  </div>
+)
 
 class About extends Component {
-  // componentDidMount() {
-  //   this.props.getUsers()
-  // }
+  componentDidMount() {
+    this.props.getUsers()
+  }
 
   render() {
-    // const { users } = this.props
+    const { users } = this.props
+
+    const chunkList = []
+    const chunk = 4
+    for (let i = 0; i < users.length; i += chunk) {
+      chunkList.push(users.slice(i, i + chunk))
+    }
+
     return (
       <div className='container'>
         <div className={styles.about}>
@@ -43,50 +56,12 @@ class About extends Component {
           <h2>
             <FormattedMessage {...messages.subtitle} />
           </h2>
-          <div className={styles.image_row}>
-            <div className={styles.member}>
-              <MemberImage picture={Carl} size={200} square round />
-              <h3>Carl Nordling</h3>
-              <h5>Head of Travel</h5>
-            </div>
-            <div className={styles.member}>
-              <MemberImage picture={Christine} size={200} square round />
-              <h3>Christine Rosquist</h3>
-              <h5>Head of Events</h5>
-            </div>
-            <div className={styles.member}>
-              <MemberImage picture={Cristian} size={200} square round />
-              <h3>Cristian Osorio Bretti</h3>
-              <h5>Head of Sales</h5>
-            </div>
-            <div className={styles.member}>
-              <MemberImage picture={Helena} size={200} square round />
-              <h3>Helena Alinder</h3>
-              <h5>Project Manager</h5>
-            </div>
-          </div>
-          <div className={styles.image_row}>
-            <div className={styles.member}>
-              <MemberImage picture={Josefin} size={200} square round />
-              <h3>Josefin Nilsson</h3>
-              <h5>Project Manager</h5>
-            </div>
-            <div className={styles.member}>
-              <MemberImage picture={Klara} size={200} square round />
-              <h3>Klara Eserstam</h3>
-              <h5>Head of Finance</h5>
-            </div>
-            <div className={styles.member}>
-              <MemberImage picture={Marko} size={200} square round />
-              <h3>Marko Lazic</h3>
-              <h5>Head of IT</h5>
-            </div>
-            <div className={styles.member}>
-              <MemberImage picture={Samuel} size={200} square round />
-              <h3>Samuel Hertzberg</h3>
-              <h5>Editor in Chief</h5>
-            </div>
-          </div>
+          {!!chunkList.length &&
+            chunkList.map((chunk, idx) => (
+              <div className={styles.image_row} key={'chunk_' + idx}>
+                {chunk.map(u => StudsMemberInfo(u))}
+              </div>
+            ))}
         </div>
         <Footer />
       </div>
@@ -94,34 +69,34 @@ class About extends Component {
   }
 }
 
-// About.propTypes = {
-//   users: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       firstName: PropTypes.string.isRequired,
-//       picture: PropTypes.string,
-//     })
-//   ),
-//   getUsers: PropTypes.func.isRequired,
-// }
+About.propTypes = {
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      firstName: PropTypes.string.isRequired,
+      lastName: PropTypes.string.isRequired,
+      userRole: PropTypes.string.isRequired,
+      picture: PropTypes.string,
+    })
+  ),
+  getUsers: PropTypes.func.isRequired,
+}
 
-// About.defaultProps = {
-//   users: [],
-// }
+About.defaultProps = {
+  users: [],
+}
 
-// function mapStateToProps(state) {
-//   const users = state.getIn(['members', 'users']).toJS()
-//   return {
-//     users: sortBy(users, ['firstName']),
-//   }
-// }
+function mapStateToProps(state) {
+  const users = state.getIn(['members', 'users']).toJS()
+  return {
+    users: sortBy(users, ['firstName']),
+  }
+}
 
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators(actions, dispatch)
-// }
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actions, dispatch)
+}
 
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(About)
-
-export default About
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About)

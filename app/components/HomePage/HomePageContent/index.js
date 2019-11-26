2064e5ad8fcad4d5f7505e3fdce130610d78d172
Anton Stagge <antonstagge95@gmail.com>
2019-11-26 10:15:27 +0100

    Add se texts

---
+++ app/components/HomePage/HomePageContent/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import messages from './messages'
+import styles from './styles.css'
---
+++ app/components/HomePage/HomePageContent/messages.js
@@ -0,0 +1,24 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  project: {
+    title: {
+      id: 'app.components.HomePage.content.project.title',
+      defaultMessage: '',
+    },
+    body: {
+      id: 'app.components.HomePage.content.project.body',
+      defaultMessage: '',
+    },
+  },
+  events: {
+    title: {
+      id: 'app.components.HomePage.content.events.title',
+      defaultMessage: '',
+    },
+    body: {
+      id: 'app.components.HomePage.content.events.body',
+      defaultMessage: '',
+    },
+  },
+})

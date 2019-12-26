a1fc4cdc0e67b1d706086a6dffd460b7d2371c29
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-12-26 13:35:19 +0100

    Fix file path issue

--- app/components/HomePage/index.js
+++ app/components/HomePage/index.js
@@ -2,6 +2,6 @@ import HomePageAbout from './HomePageAboutNotUsed'
 import HomePageHeader from './HomePageHeader'
 import HomePageContent from './HomePageContent'
 import HomePagePitch from './HomePagePitch'
-import Footer from '../Footer'
+import Footer from '../HomeFooter'
 
 export { HomePageAbout, HomePageHeader, HomePageContent, HomePagePitch, Footer }

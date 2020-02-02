67fd4580b5f7026ed1c47b0f45f67d0639197d0c
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2020-02-02 14:45:43 +0100

    Move debounce hook to utils.js

--- app/components/CompanyAmountInput/CompanyAmountInput.js
+++ app/components/CompanyAmountInput/CompanyAmountInput.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.css'
 
+import { useDebounce } from 'utils'
+
 const CompanyAmountInput = ({ currentAmount, updateAmount }) => {
   const [displayAmount, setDisplayAmount] = useState(currentAmount)
   const [amount, setAmount] = useState(currentAmount)
@@ -45,31 +47,6 @@ const CompanyAmountInput = ({ currentAmount, updateAmount }) => {
 CompanyAmountInput.propTypes = {
   currentAmount: PropTypes.number.isRequired,
   updateAmount: PropTypes.func.isRequired,
-}
-
-// Hook from https://usehooks.com/useDebounce/
-function useDebounce(value, delay) {
-  // State and setters for debounced value
-  const [debouncedValue, setDebouncedValue] = useState(value)
-
-  useEffect(
-    () => {
-      // Update debounced value after delay
-      const handler = setTimeout(() => {
-        setDebouncedValue(value)
-      }, delay)
-
-      // Cancel the timeout if value changes (also on delay change or unmount)
-      // This is how we prevent debounced value from updating if value is changed ...
-      // .. within the delay period. Timeout gets cleared and restarted.
-      return () => {
-        clearTimeout(handler)
-      }
-    },
-    [value, delay] // Only re-call effect if value or delay changes
-  )
-
-  return debouncedValue
 }
 
 export default CompanyAmountInput

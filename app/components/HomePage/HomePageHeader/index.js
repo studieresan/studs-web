2913f7e4574fed6a3440beb6b3be6adeff8f7f17
Glenn Olsson <glennholsson@gmail.com>
2020-08-20 17:56:11 +0200

    Replace Studs20 logo
    Replace with placeholder text for Studs 21

--- app/components/HomePage/HomePageHeader/index.js
+++ app/components/HomePage/HomePageHeader/index.js
@@ -5,12 +5,25 @@ import messages from './messages'
 import styles from './styles.css'
 import LogoPng from 'static/img_new/logo/logotype-white.png'
 
+//TODO: When logo exists, replace text of Studs21 with the mobile and desktop
+//logos that are commented out below
+
 function HomePageHeader() {
   return (
     <div className={styles.header}>
       <div className={styles.header_content}>
-        <img className={styles.logoDesktop} src={LogoPng} />
-        <img className={styles.logoMobile} src={LogoPng} />
+        <h1
+          className={styles.logoDesktop}
+          style={{
+            border: '5px solid white',
+            color: 'white',
+            textAlign: 'center',
+          }}
+        >
+          Studs 21
+        </h1>
+        {/* <img className={styles.logoDesktop} src={LogoPng} />
+        <img className={styles.logoMobile} src={LogoPng} /> */}
         <div className={styles.header_text}>
           <FormattedMessage {...messages.intro.about} />
         </div>

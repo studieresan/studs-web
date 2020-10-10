74f78c3db8b3a555f7a0162a6b2de0f467510716
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-10-10 17:39:52 +0200

    Set SlackComponent as name of comp

--- app/components/Slack/index.js
+++ app/components/Slack/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 const SLACK_URL =
   'https://join.slack.com/t/studs21/shared_invite/zt-gf6fn60j-sNGehAhmmyaTlaSb8VF6Xw'
 
-export class ApplicationComponent extends Component {
+export class SlackComponent extends Component {
   componentDidMount() {
     window.location.replace(SLACK_URL)
   }

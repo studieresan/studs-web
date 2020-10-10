e2ff73feb016d4346916c88198f87b989a40fbfe
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-10-10 17:17:32 +0200

    Fix slack redirect

---
+++ app/components/Slack/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+const SLACK_URL =
+  'https://join.slack.com/t/studs21/shared_invite/zt-gf6fn60j-sNGehAhmmyaTlaSb8VF6Xw'
+
+export class ApplicationComponent extends Component {
+  componentDidMount() {
+    window.location.replace(SLACK_URL)
+  }
+
+  render() {
+    return (
+      <div>
+             <a href={SLACK_URL}>Gå med här</a>
+      </div>
+    )
+  }
+}

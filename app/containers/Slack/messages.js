a1ece82d9618c0ea93a5c95bf48dc68ea4c50146
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-10-15 14:00:32 +0200

    Revert "Fix slack redirect"

--- app/containers/Slack/messages.js
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl'
-
-export default defineMessages({
-  header: {
-    id: 'app.containers.Application.header',
-    defaultMessage: 'This is the apply container',
-  },
-})
